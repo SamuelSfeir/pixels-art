0c5e867b181932794183b0b48d679f484d9e526c
Samuel Sfeir <samuel1808@hotmail.com>
2023-09-27 11:50:10 -0300

    Adiciona requisito 4

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const trocaFundoPreto = document.getElementById("corPreta")
+trocaFundoPreto.style.backgroundColor = 'black';
+
+const trocaFundoVermelho = document.getElementById("corVermelha")
+trocaFundoVermelho.style.backgroundColor = 'red';
+  
+const trocaFundoazul = document.getElementById("corAzul")
+trocaFundoazul.style.backgroundColor = 'blue';
+
+const trocaFundoVerde= document.getElementById("corVerde")
+trocaFundoVerde.style.backgroundColor = 'green';
+
+// ------------------- puxei e alterei os background (paleta e pixel) pelo js -------------------
+
+
 // Função para selecionar uma cor na paleta
 function selecionarCor(elemento) {
   // Remove a classe 'selected' de todas as cores
@@ -16,5 +31,32 @@ for (let i = 0; i < coresPaleta.length; i++) {
   const cor = coresPaleta[i];
   cor.addEventListener('click', function() {
       selecionarCor(cor);
+      selectedColor = window.getComputedStyle(cor).backgroundColor;
   });
 }
+
+// funcao:
+// passar pelos pixels
+// for
+//   if selected passar o background do selecionado pro pixel clicado (style.backgroundcolor)
+//     event.target
+
+
+//   olhar o metodo getComputedStyle
+
+
+let selectedColor = 'black'; // Default color
+
+// Function to handle pixel click event
+function paintPixel(event) {
+  const pixel = event.target;
+  pixel.style.backgroundColor = selectedColor;
+}
+
+const pixels = document.querySelectorAll('.pixel');
+pixels.forEach(pixel => {
+  pixel.addEventListener('click', paintPixel);
+});
+
+
+
