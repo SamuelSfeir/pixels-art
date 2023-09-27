cabf98c88d596dcd6f587a2f11b3d75cd517e3d7
Samuel Sfeir <samuel1808@hotmail.com>
2023-09-27 09:16:38 -0300

    Adiciona requisito 3

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-const colorPalette = document.querySelector('#color-palette');
-const pixels = document.querySelectorAll('.pixel');
+document.addEventListener('DOMContentLoaded', function() {
+  const colors = document.querySelectorAll('#color-palette');
+  const canvasSquares = document.querySelectorAll('.pixel');
 
-function changeColor(event) {
-    const selectedColor = event.target.style.backgroundColor;
-    pixels.style.backgroundColor = selectedColor;
+  let selectedColor = null;
+
+  // Adiciona o evento de clique para cada quadrado de cor
+  for (let i = 0; i < colors.length; i++) {
+    colors[i].addEventListener('click', function() {
+      selectedColor = colors[i].style.backgroundColor;
+    });
   }
 
-colorPalette.addEventListener('click', changeColor);
-
-
-// Paramos nessa parte de criar a função para pintar os quadrado
+  // Adiciona o evento de clique para cada quadrado no canvas
+  for (let i = 0; i < canvasSquares.length; i++) {
+    canvasSquares[i].addEventListener('click', function() {
+      if (selectedColor) {
+        canvasSquares[i].style.backgroundColor = selectedColor;
+      } else {
+        alert('Selecione uma cor antes de pintar o quadrado.');
+      }
+    });
+  }
+});
