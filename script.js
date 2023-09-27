c3beb907e6e8c22dff97b1e3cc084c5bbed89352
Samuel Sfeir <samuel1808@hotmail.com>
2023-09-27 10:38:00 -0300

    Adiciona requisito 3

--- script.js
+++ script.js
@@ -2,9 +2,7 @@
 function selecionarCor(elemento) {
   // Remove a classe 'selected' de todas as cores
   const cores = document.querySelectorAll('.color');
-  for (let i = 0; i < cores.length; i++) {
-      cores[i].classList.remove('selected');
-  }
+  cores.forEach(cor => cor.classList.remove('selected'));
 
   // Adiciona a classe 'selected' à cor clicada
   elemento.classList.add('selected');
@@ -13,8 +11,8 @@ function selecionarCor(elemento) {
 // Adiciona um evento de clique a cada cor na paleta
 const coresPaleta = document.querySelectorAll('.color');
 for (let i = 0; i < coresPaleta.length; i++) {
-  const cor = coresPaleta[i];
-  cor.addEventListener('click', function() {
-      selecionarCor(cor);
-  });
+    const cor = coresPaleta[i];
+    cor.addEventListener('click', function() {
+        selecionarCor(cor);
+    });
 }
